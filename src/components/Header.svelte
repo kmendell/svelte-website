792ea94570fdd03e6e116913034a26bbac41c59e
Kyle Mendell <kmendell@ofkm.us>
2025-01-26 10:02:14 -0600

    fix: add title, and add some button functions

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,6 +6,10 @@
         {name: 'About', link: '#about'},
         // {name: 'Blog', '#projects'},
     ]
+
+    function createEmail() {
+        console.log("contact me clicked")
+    }
 </script>
 
 <header
@@ -27,12 +31,12 @@
                 <p>{tab.name}</p>
             </a>
         {/each}
-        <button
+        <button on:click={createEmail}
         class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
     >
         <div
             class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
-        <h4 class="relative z-9">Get in touch</h4>
+        <h4 class="relative z-9">Contact Me</h4>
     </button>
     </div>
 </header>
